defd886ae4172c92a8e2534f669df12a41d2a987
jrodarau <joao.rodrigues.araujo@everis.com>
2020-01-13 17:26:39 -0300

    Sistema de movimentacao teclas + layout

--- vivoTest/src/app/app.module.ts
+++ vivoTest/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import 'materialize-css';
+import { MaterializeModule } from 'angular2-materialize';
+import * as $ from 'jquery';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ListfilmsComponent } from './shared/listfilms/listfilms.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListfilmsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
